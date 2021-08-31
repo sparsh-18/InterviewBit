46435f82fada8ddc42ec833b805aa256302c3447
Sparsh Verma <sparsh.red@gmail.com>
2021-08-31 16:47:17 +0530

    Update Solution.java

--- Two Pointers/Maximum Ones After Modification/Solution.java
+++ Two Pointers/Maximum Ones After Modification/Solution.java
@@ -1,3 +1,4 @@
+/*
 public class Solution {
     public int solve(ArrayList<Integer> A, int B) {
         int i=0,c=0,j=0,max=-1;
@@ -16,6 +17,27 @@ public class Solution {
             }   
             
         } 
+        return max;
+    }
+}
+*/
+
+//on revision
+public class Solution {
+    public int solve(ArrayList<Integer> A, int B) {
+        int i=0, max=0, zeroC = 0;
+
+        for(int j=0;j<A.size();j++) {
+            if(A.get(j) == 0) zeroC++;
+
+            while(zeroC > B) {
+                if(A.get(i) == 0) zeroC--;
+                i++;
+            }
+
+            max = Math.max(max, j+1-i);
+        }
+
         return max;
     }
 }
